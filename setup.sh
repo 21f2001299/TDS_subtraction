92c38026a13921869ea5683cfc81b648723e3ddd
Sandip Biswas <sandipbnvnhjv@gmail.com>
2022-07-24 19:13:32 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,8 @@
 mkdir -p ~/.streamlit/
-
 echo "\
 [general]\n\
 email = \"21f1002787@student.onlinedegree.iitm.ac.in\"\n\
 " > ~/.streamlit/credentials.toml
-
 echo "\
 [server]\n\
 headless = true\n\
