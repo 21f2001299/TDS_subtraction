37a062dd6b3e8966d2d8bb0d1c6f85b1517dd35e
Sandip Biswas <sandipbnvnhjv@gmail.com>
2022-07-24 19:09:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 import streamlit as st
 
-st.write('''
-#App for Substracting 2 Numbers
+st.header('''
+App for Substracting 2 Numbers
 ''')
 
 #Get input
 
-st.header('Number input:')
-first_num = st.number_input('FIRST_NUMBER')
-second_num = st.number_input('SECOND_NUMBER')
+st.subheader('Number input:')
+first_num = st.number_input('FIRST NUMBER')
+second_num = st.number_input('SECOND NUMBER')
   
 
 result = first_num-second_num
 
 st.subheader('Result')
-st.write(result)
+st.markdown(result)
