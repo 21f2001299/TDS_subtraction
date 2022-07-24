cd5ab1b4aa787a16678949c8e6c293822511256d
21f2001299 <109910204+21f2001299@users.noreply.github.com>
2022-07-24 20:47:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,28 @@
 import streamlit as st
+max_width: int = 1200,
+max_width_100_percent: bool = False,
+padding_top: int = 1,
+padding_right: int = 1,
+padding_left: int = 1,
+padding_bottom: int = 1,
+):
+if max_width_100_percent:
+    	max_width_str = f"max-width: 100%;"
+else:
+        max_width_str = f"max-width: {max_width}px;"
+        
+styl = f"""
+    <style>
+        .reportview-container .main .block-container{{
+            {max_width_str}
+            padding-top: {padding_top}rem;
+            padding-right: {padding_right}rem;
+            padding-left: {padding_left}rem;
+            padding-bottom: {padding_bottom}rem;
+        }}
+        }}
+    </style>
+    """
 
 st.header('''
 App for Substracting 2 Numbers
@@ -14,4 +38,4 @@ second_num = st.number_input('SECOND NUMBER')
 result = first_num-second_num
 
 st.subheader('Result')
-st.markdown(result)
+st.markdown(styl, unsafe_allow_html=True,result)
